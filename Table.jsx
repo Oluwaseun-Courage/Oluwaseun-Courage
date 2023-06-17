601ef80d8628be81dd40bc8487b0bae47254f1aa
Oluwaseun-Courage <135134433+Oluwaseun-Courage@users.noreply.github.com>
2023-06-17 15:02:34 +0100

    Add files via upload

---
+++ Table.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserTable from './UserTable';
+
+const App = () => {
+  return (
+    <div>
+      <h1>User Information</h1>
+      <UserTable />
+    </div>
+  );
+};
+export default App;
